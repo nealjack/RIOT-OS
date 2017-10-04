0c1d8b34d723b2719d913c28f48dbd78260adfc6
Hyungsin <hskim@berkeley.edu>
2017-10-03 18:43:26 -0700

    fix response to pending ACK

--- pkg/openthread/contrib/platform_radio.c
+++ pkg/openthread/contrib/platform_radio.c
@@ -219,6 +219,7 @@ void send_pkt(otInstance *aInstance, netdev_t *dev, netdev_event_t event)
             break;
         case NETDEV_EVENT_TX_COMPLETE_DATA_PENDING:
             DEBUG("openthread: NETDEV_EVENT_TX_COMPLETE_DATA_PENDING\n\n");
+            ackFrame = _create_fake_ack_frame(true);
             otPlatRadioTxDone(aInstance, &sTransmitFrame, &ackFrame, OT_ERROR_NONE);
             break;
         case NETDEV_EVENT_TX_NOACK:
